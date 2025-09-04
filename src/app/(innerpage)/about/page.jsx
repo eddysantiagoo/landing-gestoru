7a5cd842860389c226b541b4f665b88dccffb88b
Eddy Santiago <eddysantiagogh@gmail.com>
2025-09-04 13:07:24 -0500

    Revise About page content and structure for better clarity and engagement

--- src/app/(innerpage)/about/page.jsx
+++ src/app/(innerpage)/about/page.jsx
@@ -5,42 +5,45 @@ import Choose4 from "../../Components/Choose/Choose4";
 import HowWork from "../../Components/HowWork/HowWork";
 
 const page = () => {
+  const featureList = [
+    "Software en la nube para PYMES",
+    "Suite completa de herramientas",
+    "Gestión integral de negocios",
+    "Soporte especializado"
+  ];
+
   return (
     <div>
       <BreadCumb
         bgimg="/assets/images/bg/breadcumgBg.png"
-        Title="About Us"
+        Title="Quiénes Somos"
       ></BreadCumb>
+
       <About4
         img1="/assets/images/about/aboutThumb1_1.png"
         img2="/assets/images/about/aboutThumb1_2.png"
-        subtitle="About Our App"
-        title="Simple Reports & Analytics Backdown As it"
-        content="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use"
-        FeatureList={[
-          "With our Technological and Marketing Solutions.",
-          "We are trusted all over the world.",
-          "Start Your 14 Days Free Trials Today!",
-        ]}
-        btnname="Discover More"
-        btnurl="/about"
-      ></About4>
+        subtitle="Gestoru"
+        title="Quiénes Somos"
+        content="En Gestoru ayudamos a gestionar y estructurar tu PYME o tu negocio a través de nuestro software en la nube. Una suite de herramientas para llevar a otro nivel tu emprendimiento."
+        FeatureList={featureList}
+        btnname="Contactar por WhatsApp"
+        btnurl="https://wa.me/1234567890?text=Hola,%20me%20interesa%20conocer%20más%20sobre%20Gestoru"
+      />
+
       <Choose2
         img1="/assets/images/wcu/wcuThumb2_1.png"
         img2="/assets/images/wcu/wcuThumb2_2.png"
         img3="/assets/images/wcu/wcuThumb2_3.png"
-        subtitle="Customizations & Analysis"
-        title="Manage your Team's Easily Communication."
-        content="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even"
+        subtitle="Soluciones Personalizadas"
+        title="Gestiona tu negocio de manera eficiente"
+        content="Ofrecemos herramientas especializadas para optimizar los procesos de tu empresa, desde la facturación hasta el control de inventarios, todo en una plataforma integrada."
         boximg1="/assets/images/icon/wcuIcon2_1.svg"
-        boxtitle1="Easy Collaboration"
-        boxcontent1="There are many variations of passages of Lorem Ipsum available, but the majority have"
+        boxtitle1="Fácil Colaboración"
+        boxcontent1="Nuestro software permite que tu equipo trabaje de manera coordinada y eficiente"
         boximg2="/assets/images/icon/wcuIcon2_2.svg"
-        boxtitle2="Innovative Solutions"
-        boxcontent2="There are many variations of passages of Lorem Ipsum available, but the majority have"
+        boxtitle2="Soluciones Innovadoras"
+        boxcontent2="Tecnología de vanguardia adaptada a las necesidades de tu negocio"
       ></Choose2>
-      <HowWork></HowWork>
-      <Choose4></Choose4>
     </div>
   );
 };
